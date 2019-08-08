b7f6a683091148376db44cca05a3f3ba8fea2930
Rushani Shawuti <ruhsane924@gmail.com>
2019-08-07 23:00:49 -0700

    ui set up done

---
+++ WeatherMood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherMood
+//
+//  Created by Ruhsane Sawut on 8/7/19.
+//  Copyright © 2019 ruhsane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
