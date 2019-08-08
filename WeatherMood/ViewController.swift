b34bf9a592d9d85c3a0ceb828851a23ce8d1bcb6
Rushani Shawuti <ruhsane924@gmail.com>
2019-08-07 23:50:00 -0700

    access authorization for user location

--- WeatherMood/ViewController.swift
+++ WeatherMood/ViewController.swift
@@ -7,21 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
 import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var moodInputField: UITextField!
     @IBAction func submitButton(_ sender: UIButton) {
         moodOutputField.text = "Your mood today is: " + moodInputField.text!
     }
     @IBOutlet weak var moodOutputField: UILabel!
-    
+    @IBOutlet weak var weatherOutput: UILabel!
+    let locationManager = CLLocationManager()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Ask for location Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
     
+    private func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) -> (Int, Int)? {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return nil }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        return (Int(locValue.latitude), Int(locValue.longitude))
     }
 
 
